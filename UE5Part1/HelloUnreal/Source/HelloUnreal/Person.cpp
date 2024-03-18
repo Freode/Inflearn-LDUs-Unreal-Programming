299b08cbaf0eda6fb9441304ab70ca9332d2ed3d
Freode <free4760@naver.com>
2024-03-18 21:49:49 +0900

    Study Part 1 - 6

---
+++ UE5Part1/HelloUnreal/Source/HelloUnreal/Person.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Person.h"
+
+// 생성자
+UPerson::UPerson()
+{
+	Name = TEXT("홍길동");
+	Year = 1;
+}
+
+void UPerson::DoLesson()
+{
+	UE_LOG(LogTemp, Log, TEXT("%s님이 수업에 참여합니다."), *Name);
+}
+
+const FString& UPerson::GetName() const
+{
+	// TODO: insert return statement here
+	return Name;
+}
+
+void UPerson::SetName(const FString& InName)
+{
+	Name = InName;
+}
