0d1bb9f0976c9fc8b9d90fcd0085319328b0bba1
Freode <free4760@naver.com>
2025-02-23 12:28:57 +0900

    Complete Part 2-1

---
+++ UE5Part2/ArenaBattle/Source/ArenaBattle/Game/ABGameMode.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/ABGameMode.h"
+#include "Player/ABPlayerController.h"
+
+
+AABGameMode::AABGameMode()
+{
+	// C++ 클래스가 아닌 것은 우리가 애셋으로부터 정보를 가져와야 함.
+	static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+
+	if (ThirdPersonClassRef.Class)
+	{
+		DefaultPawnClass = ThirdPersonClassRef.Class;
+	}
+
+	// C++ 클래스는 이렇게 정보를 가져올 수 있음.
+	PlayerControllerClass = AABPlayerController::StaticClass();
+
+	// C++ 클래스에 대한 헤더 파일을 include 하지 않고 사용
+	// 기본으로 언리얼 클래스가 복제된 것이므로 가능 
+	//static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerRef(TEXT("/Script/ArenaBattle.ABPlayerController'"));
+	//if (PlayerControllerRef.Class)
+	//{
+	//	PlayerControllerClass = PlayerControllerRef.Class;
+	//}
+
+}
